4a9ed14697c7ff419c9d71d6281193a901d5196e
Prateek Joshi <pjoshi22900@gmail.com>
2021-11-04 11:52:47 +0530

    Added normalization layer

--- GAN/discriminator.py
+++ GAN/discriminator.py
@@ -5,6 +5,9 @@ class Discriminator(keras.models.Model):
     def __init__(self, **kwargs) -> None:
         super(Discriminator, self).__init__(**kwargs)
 
+        # coerce inputs into [0,1] space
+        self.norm = keras.layers.Normalization(axis=None, name='Normalize')
+
         self.conv1 = keras.layers.Conv2D(64, (5,5), strides=(2,2), padding='same', input_shape=(128,128,1), name='Conv1')
         self.pool1 = keras.layers.MaxPool2D(name='MaxPool1')
         self.actv1 = keras.layers.LeakyReLU(name='ReLU1')
@@ -19,7 +22,8 @@ class Discriminator(keras.models.Model):
         self.op = keras.layers.Dense(1, activation='sigmoid')
 
     def call(self, input, training=False):
-        x = self.conv1(input)
+        x = self.norm(input)
+        x = self.conv1(x)
         x = self.pool1(x)
         x = self.actv1(x)
         if training:
