90b446b25fb483d6d6c416265aedf78cce0a863f
Prateek Joshi <pjoshi22900@gmail.com>
2021-11-08 23:56:29 +0530

    Update training loop

--- GAN/gan.py
+++ GAN/gan.py
@@ -1,7 +1,6 @@
 import tensorflow as tf 
 from tensorflow import keras
 import numpy as np
-tf.config.run_functions_eagerly(True)
 
 
 class GAN(keras.Model):
@@ -17,35 +16,33 @@ class GAN(keras.Model):
         self.g_optimizer = g_optimizer
         self.loss_fn = keras.losses.get(loss_fn)
     
+    def discriminator_loss(self, real_output, fake_output):
+        real_loss = self.loss_fn(tf.ones_like(real_output), real_output)
+        fake_loss = self.loss_fn(tf.zeros_like(fake_output), fake_output)
+        total_loss = real_loss + fake_loss
+        return total_loss
+
+    def generator_loss(self, fake_output):
+        return self.loss_fn(tf.ones_like(fake_output), fake_output)
+
     def train_step(self, data):
-        real_images = data
-        tf.cast(real_images, dtype=tf.float32)
-        batch_size = tf.shape(real_images)[0]
+        tf.cast(data, dtype=tf.float32)
+        batch_size = tf.shape(data)[0]
         random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim), dtype=tf.float32)   # Gaussian Noise
         
-        generated_images = self.generator(random_latent_vectors)
-        combined_images = tf.concat([generated_images.numpy(), real_images.numpy()], axis=0)
+        with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+            generated_images = self.generator(random_latent_vectors, training=True)
+
+            real_output = self.discriminator(data, training=True)
+            fake_output = self.discriminator(generated_images, training=True)
+
+            gen_loss = self.generator_loss(fake_output)
+            disc_loss = self.discriminator_loss(real_output, fake_output)
+
+        gradients_of_generator = gen_tape.gradient(gen_loss, self.generator.trainable_variables)
+        gradients_of_discriminator = disc_tape.gradient(disc_loss, self.discriminator.trainable_variables)
+
+        self.g_optimizer.apply_gradients(zip(gradients_of_generator, self.generator.trainable_variables))
+        self.d_optimizer.apply_gradients(zip(gradients_of_discriminator, self.discriminator.trainable_variables))
         
-        labels = tf.concat([tf.ones((batch_size, 1)),
-                            tf.zeros((batch_size, 1))], axis=0)
-        
-        labels += 0.05 * tf.random.uniform(tf.shape(labels))
-        
-        with tf.GradientTape() as tape:
-            predictions = self.discriminator(combined_images, training=True)
-            d_loss = self.loss_fn(labels, predictions)
-            
-        grads = tape.gradient(d_loss, self.discriminator.trainable_weights)
-        self.d_optimizer.apply_gradients(zip(grads, self.discriminator.trainable_weights))
-        
-        random_latent_vectors = tf.random.normal(shape=(batch_size, self.latent_dim))
-        misleading_labels = tf.zeros((batch_size, 1))
-        
-        with tf.GradientTape() as tape:
-            predictions = self.discriminator(self.generator(random_latent_vectors))
-            g_loss = self.loss_fn(misleading_labels, predictions)
-            
-        grads = tape.gradient(g_loss, self.generator.trainable_weights)
-        self.g_optimizer.apply_gradients(zip(grads, self.generator.trainable_weights))
-        
-        return {"discriminator_loss": d_loss, "generator_loss": g_loss}
+        return {"discriminator_loss": disc_loss, "generator_loss": gen_loss}
